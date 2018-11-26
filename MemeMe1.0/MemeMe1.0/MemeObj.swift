c5f089c6e7da36992821dedd71d71a07cc942253
Nora <nora.al-mansour@hotmail.com>
2018-11-26 22:37:47 +0300

    Add files via upload

---
+++ MemeMe1.0/MemeMe1.0/MemeObj.swift
@@ -0,0 +1,18 @@
+//
+//  MemeObj.swift
+//  MemeMe1.0
+//
+//  Created by Nora al-mansour on 3/7/1440 AH.
+//  Copyright © 1440 Nora al-mansour. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: struct for meme
+struct MemeObject {
+    var topText: String!
+    var bottomText: String!
+    var originalImage: UIImage!
+    var memedImage : UIImage!
+}
